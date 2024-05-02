f72e240550e8f29372fabe788bbac0977150a014
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2024-05-02 16:54:46 +0530

    new

---
+++ JS/1.js
@@ -0,0 +1,66 @@
+console.log("munjal");
+
+// js is a light weight programming language and scripting language (client side scriping language)
+
+// we are using it to improve functionnality and behaviour 
+
+// the best practice to add script tag in html file is -> jabb body ka sara code khatam ho jaye tab code ke badd likhna hai script tag 
+// bcz jab last m likhege to har chiz pass ho chuki hogi and it will take less time to render (to increase user experience )
+
+// hum (node 1.js) ke dwarea bhi js file ko run kar sakete hai in terminal 
+
+
+////////////////////// do java v/s javascript
+
+
+
+// let v/s var keyword
+
+let a = 1;
+// let a = 3; // not possible (dubara let a = 5 define karna is not possible)
+console.log(a);
+
+// but through var it is possible
+var b = 2;
+var b = 10; // it is possible using var 
+console.log(b);
+
+
+
+if(true){
+    let r =11;
+    console.log(r);
+}
+
+// console.log(r); // error(r is not defined) as let ka scope sirf {} brackets taak hi hota hai
+
+
+if(true){
+    var t = 9;
+    console.log(t);
+}
+
+console.log(t);
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     {int a = 5;}
+    
+//     // cout<<b; //  compiler
+
+//     cout<<a; // runtime pr hi bta diya
+
+//     return 0;
+
+// }
+
+
+
+
+
+
